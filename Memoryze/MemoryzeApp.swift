daa06e3114156152e869e417ae5a0fec87291816
Marcelo Macedo <marcelomacedodev@gmail.com>
2022-08-25 18:43:15 +0100

    Initial Commit

---
+++ Memoryze/MemoryzeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryzeApp.swift
+//  Memoryze
+//
+//  Created by Marcelo Macedo on 25/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct MemoryzeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
