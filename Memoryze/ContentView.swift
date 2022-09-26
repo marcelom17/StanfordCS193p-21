75a952fc92b498bc2d9b256ec98e556622d24f09
Marcelo Macedo <marcelomacedodev@gmail.com>
2022-09-26 19:15:30 +0100

    [NEW] Homework Lesson 2

--- Memoryze/ContentView.swift
+++ Memoryze/ContentView.swift
@@ -8,14 +8,106 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var emojiCount = 4
+    let vehiclesArray = ["🚗", "🚌", "🛻", "🏎", "🚜", "🏍", "🚚", "🚒", "🚲", "🚐", "🚅", "🚂", "✈️", "🚀", "🚁", "⛵️", "🛳", "🚤", "🛺", "🚃"]
+    let animalsArray = ["🐶", "🐱", "🐭", "🦊", "🐻", "🐼", "🐻‍❄️", "🐨", "🐯", "🦁", "🐮", "🐷", "🐸", "🐵", "🐔", "🐧", "🐦", "🐤", "🦅", "🐝", "🐺", "🐙", "🐴", "🐗", "🦐", "🐳", "🐠", "🐟", "🦀", "🦞"]
+    let fruitsArray = ["🍎", "🍏", "🍐", "🍊", "🍋", "🍌", "🍉", "🍇", "🍓", "🫐", "🍈", "🍒", "🍑", "🥭", "🍍", "🥥", "🥝", "🍅", "🍆", "🥑"]
+    
+    @State var gridArray = [String]()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Memorize!").font(.largeTitle)
+            ScrollView {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]) {
+                    ForEach(gridArray[0..<gridArray.count], id: \.self) { val in
+                        CardView(content: val).aspectRatio(2/3, contentMode: .fit )
+                     }
+                }
+            }
+            .foregroundColor(.red)
+            Spacer()
+            HStack{
+                vehicles
+                Spacer()
+                fruits
+                Spacer()
+                animals
+                
+            }
+            .padding(.horizontal)
+            .font(.largeTitle)
+        }
+        .padding(.horizontal)
+    }
+    
+    var vehicles: some View{
+        Button {
+            //Change array
+            print("Change Array To Vehicles")
+            gridArray = vehiclesArray.shuffled()
+        } label: {
+            VStack {
+                Image(systemName: "car")
+                Text("Vehicles").font(.body)
+            }
+        }
+    }
+    
+    var fruits: some View{
+        Button {
+            //Change array
+            print("Change Array To Fruits")
+            gridArray = fruitsArray.shuffled()
+        } label: {
+            VStack {
+                Image(systemName: "camera.macro")
+                Text("Fruits").font(.body)
+            }
+        }
+    }
+    
+    var animals: some View{
+        Button {
+            //Change array
+            print("Change Array To Animals")
+            gridArray = animalsArray.shuffled()
+        } label: {
+            VStack {
+                Image(systemName: "tortoise.fill")
+                Text("Animals").font(.body)
+            }
+        }
+    }
+    
+}
+
+struct CardView : View {
+    
+    @State var isFaceUp = false
+    var content : String
+    
+    var body: some View{
+        ZStack {
+            let shape = RoundedRectangle(cornerRadius: 25)
+            if isFaceUp{
+                shape.fill().foregroundColor(.white)
+                shape.strokeBorder(lineWidth: 3)
+                Text(content).font(.largeTitle)
+            }
+            else {
+                shape.fill()
+            }
+        }
+        .onTapGesture {
+            self.isFaceUp = !self.isFaceUp
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.light)
     }
 }
